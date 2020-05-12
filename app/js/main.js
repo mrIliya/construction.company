9488fe1b0af90676ce5c0235ff3351f6eaf00ba0
mriliya <mr.litvishchenko@gmail.com>
2020-05-12 22:09:51 +0300

    finished html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,62 @@
-$(function(){
+$(function () {
+
+	$('.projects__content .menu-btn').on('click', function (event) {
+		var id = $(this).attr('data-id');
+
+		$('.projects-gallery').find('.projects-gallery__item').removeClass('active').hide();
+
+		$('.projects__menu').find('.menu-btn').removeClass('active');
 
 
+		$(this).addClass('active');
 
-  
+		$('#' + id).addClass('active').fadeIn(1000, 'linear');
+
+		return false;
+
+	});
+
+	$('.reviews__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: '60px',
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		pauseOnHover: true
+	});
+
+	$("[data-scroll]").on('click', function () {
+		event.preventDefault();
+
+		let elementId = $(this).data('scroll');
+
+		let elementOffset = $(elementId).offset().top;
+
+
+		$("html, body").animate({
+			scrollTop: elementOffset
+
+		}, 700
+		);
+	});
+
+	let menu = $('.header-menu');
+	let header = $('#top');
+	let headerH = header.innerHeight();
+	let scrollPos = $(window).scrollTop();
+
+	$(window).on('scroll load resize', function () {
+		headerH = header.innerHeight();
+		scrollPos = $(this).scrollTop();
+
+		if (scrollPos > headerH) {
+			menu.addClass('fixed');
+
+		} else { menu.removeClass('fixed'); }
+
+	});
+
+
 });
